08b8f9f45fa7227d104dd14f5085d7da26a17cfc
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-09-10 09:45:38 -0700

    All Done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace LINQ
 {
     public class Program
-    {
+    { /*Use the list of Linq methods for help
+
+        Create a list of video game names...
+        Order the list of games by length of the game name.
+        Use the lambda expression in this exercise as well.
+        use Method Syntax for this exercise */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string> { "Halo", "Call of Duty", "Fortnite", "Minecraft", "GTA", "Red Dead Redemption", "Assassins Creed", "FIFA", "NBA 2K", "Madden" };
+
+            var orderedGames = games.OrderBy(g => g.Length);
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
+            
         }
     }
 }
